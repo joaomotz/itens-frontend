9e80560fa64755026f5d6858ec7a1bef4410a992
Joaomotz <joao.vmcarvalho6@senacsp.edu.br>
2022-06-27 15:21:22 -0300

    [+]roteamento de páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import UsuariosPage from './Pages/Usuarios';
 
 const App = function() {
   return (
-    <></>
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/Usuarios" element={<UsuariosPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Pages/Usuarios.tsx
@@ -0,0 +1,15 @@
+const UsuariosPage = function () {
+    return(
+        <div>
+        <h1>Usuários</h1>
+        <ul>
+            <li>Usuário 1</li>
+            <li>Usuário 2</li>
+            <li>Usuário 3</li>
+        </ul>
+        <button>Novo</button>
+        </div>
+    );
+};
+
+export default UsuariosPage;
